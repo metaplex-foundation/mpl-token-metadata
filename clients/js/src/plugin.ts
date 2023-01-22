b059cdc919f9ceec5310897c6c23652d634bdc9e
Loris Leiva <loris.leiva@gmail.com>
2023-01-22 21:35:20 +0000

    wip

--- clients/js/src/plugin.ts
+++ clients/js/src/plugin.ts
@@ -1,20 +1,8 @@
 import { MetaplexPlugin } from '@lorisleiva/js-core';
-import {
-  getMplSystemExtrasProgram,
-  getMplTokenExtrasProgram,
-  getSplAssociatedTokenProgram,
-  getSplMemoProgram,
-  getSplSystemProgram,
-  getSplTokenProgram,
-} from './generated';
+import { getMplTokenMetadataProgram } from './generated';
 
 export const mplDigitalAsset = (): MetaplexPlugin => ({
   install(metaplex) {
-    metaplex.programs.add(getSplSystemProgram(), false);
-    metaplex.programs.add(getSplMemoProgram(), false);
-    metaplex.programs.add(getSplTokenProgram(), false);
-    metaplex.programs.add(getSplAssociatedTokenProgram(), false);
-    metaplex.programs.add(getMplSystemExtrasProgram(), false);
-    metaplex.programs.add(getMplTokenExtrasProgram(), false);
+    metaplex.programs.add(getMplTokenMetadataProgram(), false);
   },
 });
