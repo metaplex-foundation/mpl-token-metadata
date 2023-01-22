a1f1e94e2da2882a3de33b40f2f0e69d1a850b19
Loris Leiva <loris.leiva@gmail.com>
2023-01-22 21:32:19 +0000

    wip

--- infra/validator.cjs
+++ infra/validator.cjs
@@ -4,20 +4,28 @@ const programDir = path.join(__dirname, "..", "programs");
 function getProgram(dir, programName) {
   return path.join(programDir, dir, "target", "deploy", programName);
 }
+function getExternalProgram(dir, programName) {
+  return path.join(__dirname, "external-programs", programName);
+}
 
 module.exports = {
   validator: {
     commitment: "processed",
     programs: [
       {
+        label: "Token Metadata",
+        programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+        deployPath: getProgram("token-metadata", "mpl_token_metadata.so"),
+      },
+      {
         label: "System Extras",
         programId: "SysExL2WDyJi9aRZrXorrjHJut3JwHQ7R9bTyctbNNG",
-        deployPath: getProgram("system-extras", "mpl_system_extras.so"),
+        deployPath: getExternalProgram("mpl_system_extras.so"),
       },
       {
         label: "Token Extras",
         programId: "TokExjvjJmhKaRBShsBAsbSvEWMA1AgUNK7ps4SAc2p",
-        deployPath: getProgram("token-extras", "mpl_token_extras.so"),
+        deployPath: getExternalProgram("mpl_token_extras.so"),
       },
     ],
   },
