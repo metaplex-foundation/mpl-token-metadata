b28b16395f4a0fae5d4726fdcaca95941b6f02e9
Loris Leiva <loris.leiva@gmail.com>
2023-01-24 19:49:26 +0000

    wip

--- clients/js/test/Create.test.ts
+++ clients/js/test/Create.test.ts
@@ -1,13 +1,44 @@
-import { generateSigner, transactionBuilder } from '@lorisleiva/js-test';
+import {
+  generateSigner,
+  none,
+  some,
+  transactionBuilder,
+} from '@lorisleiva/js-test';
 import test from 'ava';
-import { create } from '../src';
+import { create, createArgs, CreateArgs, TokenStandard } from '../src';
 import { createMetaplex } from './_setup';
 
-test('it TODO', async (t) => {
-  //
+test('it can create a new NFT with minimum configuration', async (t) => {
+  // Given
   const mx = await createMetaplex();
   const mint = generateSigner(mx);
 
   // When
-  await transactionBuilder(mx).add(create(mx, { mint })).sendAndConfirm();
+  await transactionBuilder(mx)
+    .add(
+      create(mx, {
+        mint,
+        createArgs: createArgs('V1', <CreateArgs>{
+          updateAuthority: mx.identity.publicKey,
+          name: 'My NFT',
+          uri: 'https://example.com/my-nft.json',
+          sellerFeeBasisPoints: 500,
+          creators: some([
+            {
+              address: mx.identity.publicKey,
+              verified: true,
+              share: 100,
+            },
+          ]),
+          tokenStandard: TokenStandard.NonFungible,
+          collection: none(),
+          uses: none(),
+          collectionDetails: none(),
+          ruleSet: none(),
+          decimals: none(),
+          printSupply: none(),
+        }),
+      })
+    )
+    .sendAndConfirm();
 });
