77a40cefd1ef4999a0c9bd9acddaaa080b70eca9
Loris Leiva <loris.leiva@gmail.com>
2023-01-28 19:58:04 +0000

    wip

--- clients/js/src/instructions/_createHelper.ts
+++ clients/js/src/instructions/_createHelper.ts
@@ -1,86 +1,25 @@
-import {
-  Amount,
-  Option,
-  PublicKey,
-  some,
-  WrappedInstruction,
-} from '@lorisleiva/js-core';
-import {
-  Collection,
-  CreateInstructionAccounts,
-  Creator,
-  create,
-  createArgs,
-  PrintSupplyArgs,
-  TokenStandard,
-  UsesArgs,
-  CollectionDetailsArgs,
-} from '../generated';
+import { Option, some, WrappedInstruction } from '@lorisleiva/js-core';
+import { createV1, Creator } from '../generated';
 
 // Inputs.
-export type CreateHelperArgs = CreateInstructionAccounts & {
-  name: string;
-  symbol?: string;
-  uri: string;
-  sellerFeeBasisPoints: Amount<'%', 2>;
-  creators?: Array<Creator>;
-  primarySaleHappened?: boolean;
-  isMutable?: boolean;
-  tokenStandard: TokenStandard;
-  collection?: Option<Collection>;
-  uses?: Option<UsesArgs>;
-  collectionDetails?: Option<CollectionDetailsArgs>;
-  ruleSet?: Option<PublicKey>;
-  decimals?: Option<number>;
-  printSupply?: Option<PrintSupplyArgs>;
-};
+export type CreateHelperArgs = Omit<
+  Parameters<typeof createV1>[1],
+  'creators'
+> & { creators?: Option<Array<Creator>> };
 
 export const createHelper = (
-  context: Parameters<typeof create>[0],
+  context: Parameters<typeof createV1>[0],
   input: CreateHelperArgs
-): WrappedInstruction => {
-  const {
-    name,
-    symbol,
-    uri,
-    sellerFeeBasisPoints,
-    creators,
-    primarySaleHappened,
-    isMutable,
-    tokenStandard,
-    collection,
-    uses,
-    collectionDetails,
-    ruleSet,
-    decimals,
-    printSupply,
-    ...accounts
-  } = input;
-  return create(context, {
-    ...accounts,
-    createArgs: createArgs('V1', {
-      name,
-      symbol,
-      uri,
-      sellerFeeBasisPoints,
-      creators: some(
-        creators ?? [
-          {
-            address: accounts.updateAuthority ?? context.identity.publicKey,
-            share: 100,
-            verified: true,
-          },
-        ]
-      ),
-      primarySaleHappened,
-      isMutable,
-      tokenStandard,
-      collection,
-      uses,
-      collectionDetails,
-      ruleSet,
-      decimals,
-      printSupply,
-    }),
+): WrappedInstruction =>
+  createV1(context, {
+    ...input,
+    creators:
+      input.creators ??
+      some([
+        {
+          address: input.updateAuthority ?? context.identity.publicKey,
+          share: 100,
+          verified: true,
+        },
+      ]),
   });
-};
