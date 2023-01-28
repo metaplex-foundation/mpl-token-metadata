aaaaf1f9f95f9c860292ef0a03fadbbdab520da6
Loris Leiva <loris.leiva@gmail.com>
2023-01-28 20:29:32 +0000

    wip

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -1,6 +1,32 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { createMetaplex as baseCreateMetaplex } from '@lorisleiva/js-test';
-import { mplDigitalAsset } from '../src';
+import {
+  createMetaplex as baseCreateMetaplex,
+  generateSigner,
+  Metaplex,
+  percentAmount,
+  Signer,
+  transactionBuilder,
+} from '@lorisleiva/js-test';
+import { mplDigitalAsset, createNft as baseCreateNft } from '../src';
 
 export const createMetaplex = async () =>
   (await baseCreateMetaplex()).use(mplDigitalAsset());
+
+export const createNft = async (
+  mx: Metaplex,
+  input: Partial<Parameters<typeof baseCreateNft>[1]> = {}
+): Promise<Signer> => {
+  const mint = generateSigner(mx);
+  await transactionBuilder(mx)
+    .add(
+      baseCreateNft(mx, {
+        mint,
+        name: 'My NFT',
+        uri: 'https://example.com',
+        sellerFeeBasisPoints: percentAmount(2.5),
+        ...input,
+      })
+    )
+    .sendAndConfirm();
+  return mint;
+};
--- clients/js/test/updateV1.test.ts
+++ clients/js/test/updateV1.test.ts
@@ -1,37 +1,16 @@
-import {
-  generateSigner,
-  percentAmount,
-  some,
-  transactionBuilder,
-} from '@lorisleiva/js-test';
+import { some, transactionBuilder } from '@lorisleiva/js-test';
 import test from 'ava';
-import {
-  createNft,
-  fetchMetadata,
-  findMetadataPda,
-  Metadata,
-  updateV1,
-} from '../src';
-import { createMetaplex } from './_setup';
+import { fetchMetadata, findMetadataPda, Metadata, updateV1 } from '../src';
+import { createMetaplex, createNft } from './_setup';
 
 test('it can update a NonFungible', async (t) => {
-  // Given
+  // Given an existing NFT.
   const mx = await createMetaplex();
-  const mint = generateSigner(mx);
-  await transactionBuilder(mx)
-    .add(
-      createNft(mx, {
-        mint,
-        name: 'NFT #1',
-        uri: 'https://example.com',
-        sellerFeeBasisPoints: percentAmount(2.5),
-      })
-    )
-    .sendAndConfirm();
+  const mint = await createNft(mx, { name: 'NFT #1' });
   const initialMetadata = findMetadataPda(mx, { mint: mint.publicKey });
   const initialMetadataAccount = await fetchMetadata(mx, initialMetadata);
 
-  // When
+  // When we update the name of the NFT.
   await transactionBuilder(mx)
     .add(
       updateV1(mx, {
@@ -41,7 +20,7 @@ test('it can update a NonFungible', async (t) => {
     )
     .sendAndConfirm();
 
-  // Then
+  // Then the account data was updated.
   const updatedMetadataAccount = await fetchMetadata(mx, initialMetadata);
   t.like(updatedMetadataAccount, <Metadata>{ name: 'NFT #2' });
 });
