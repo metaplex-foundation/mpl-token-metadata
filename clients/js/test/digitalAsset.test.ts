60facb3f1320c75150ca33385c763eb6d9562431
Loris Leiva <loris.leiva@gmail.com>
2023-02-01 14:48:53 +0000

    wip

--- clients/js/test/digitalAsset.test.ts
+++ clients/js/test/digitalAsset.test.ts
@@ -1,6 +1,7 @@
 import {
   base58PublicKey,
   generateSigner,
+  PublicKey,
   publicKey,
   some,
 } from '@lorisleiva/js-test';
@@ -8,6 +9,7 @@ import test from 'ava';
 import {
   DigitalAsset,
   fetchAllDigitalAsset,
+  fetchAllDigitalAssetByCreator,
   fetchAllDigitalAssetByUpdateAuthority,
   fetchDigitalAsset,
   fetchDigitalAssetByMetadata,
@@ -88,6 +90,57 @@ test('it can fetch all DigitalAssets by mint list', async (t) => {
   t.true(mints.includes(base58PublicKey(mintB.publicKey)));
 });
 
+test('it can fetch all DigitalAssets by creators', async (t) => {
+  // Given two creators A and B.
+  const mx = await createMetaplex();
+  const creatorA = generateSigner(mx).publicKey;
+  const creatorB = generateSigner(mx).publicKey;
+
+  // And three NFTs such that 2 are created by A and 1 is created by B.
+  const mintA1 = await createDigitalAssetWithFirstCreator(mx, creatorA);
+  const mintA2 = await createDigitalAssetWithFirstCreator(mx, creatorA);
+  const mintB1 = await createDigitalAssetWithFirstCreator(mx, creatorB);
+
+  // When we fetch all digital assets such that their first creator is A.
+  const digitalAssets = await fetchAllDigitalAssetByCreator(mx, creatorA);
+
+  // Then we get the two NFTs created by A.
+  t.is(digitalAssets.length, 2);
+  const mints = digitalAssets.map((da) => base58PublicKey(da.mint));
+  t.true(mints.includes(base58PublicKey(mintA1.publicKey)));
+  t.true(mints.includes(base58PublicKey(mintA2.publicKey)));
+
+  // And we don't get the NFT created by B.
+  t.false(mints.includes(base58PublicKey(mintB1.publicKey)));
+});
+
+test('it can fetch all DigitalAssets by creators in different positions', async (t) => {
+  // Given two creators A and B.
+  const mx = await createMetaplex();
+  const creatorA = generateSigner(mx).publicKey;
+  const creatorB = generateSigner(mx).publicKey;
+
+  // And three NFTs such that 2 are co-created by A and 1 is co-created by B.
+  // Creators A and B are assigned as the second creator for each of their NFTs.
+  const mintA1 = await createDigitalAssetWithSecondCreator(mx, creatorA);
+  const mintA2 = await createDigitalAssetWithSecondCreator(mx, creatorA);
+  const mintB1 = await createDigitalAssetWithSecondCreator(mx, creatorB);
+
+  // When we fetch all digital assets such that their second creator is A.
+  const digitalAssets = await fetchAllDigitalAssetByCreator(mx, creatorA, {
+    position: 2,
+  });
+
+  // Then we get the two NFTs co-created by A.
+  t.is(digitalAssets.length, 2);
+  const mints = digitalAssets.map((da) => base58PublicKey(da.mint));
+  t.true(mints.includes(base58PublicKey(mintA1.publicKey)));
+  t.true(mints.includes(base58PublicKey(mintA2.publicKey)));
+
+  // And we don't get the NFT co-created by B.
+  t.false(mints.includes(base58PublicKey(mintB1.publicKey)));
+});
+
 test('it can fetch all DigitalAssets by update authority', async (t) => {
   // Given two update authorities A and B.
   const mx = await createMetaplex();
@@ -123,3 +176,27 @@ test('it can fetch all DigitalAssets by update authority', async (t) => {
   // And we don't get the NFT maintained by B.
   t.false(mints.includes(base58PublicKey(mintB1.publicKey)));
 });
+
+function createDigitalAssetWithFirstCreator(
+  context: Parameters<typeof createDigitalAsset>[0],
+  creator: PublicKey
+) {
+  return createDigitalAsset(context, {
+    creators: some([
+      { address: creator, share: 50, verified: false },
+      { address: context.identity.publicKey, share: 50, verified: false },
+    ]),
+  });
+}
+
+function createDigitalAssetWithSecondCreator(
+  context: Parameters<typeof createDigitalAsset>[0],
+  creator: PublicKey
+) {
+  return createDigitalAsset(context, {
+    creators: some([
+      { address: context.identity.publicKey, share: 50, verified: false },
+      { address: creator, share: 50, verified: false },
+    ]),
+  });
+}
