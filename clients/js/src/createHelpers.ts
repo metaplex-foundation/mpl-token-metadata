01b32f72b7a55b3d62d8a0a44779fc1a5eb0b528
Loris Leiva <loris.leiva@gmail.com>
2023-02-05 22:55:23 +0000

    wip

---
+++ clients/js/src/createHelpers.ts
@@ -0,0 +1,51 @@
+import type { WrappedInstruction } from '@lorisleiva/js-core';
+import { TokenStandard } from './generated';
+import {
+  createV1,
+  CreateV1InstructionInput,
+  mintV1,
+  MintV1InstructionInput,
+} from './instructions';
+
+export const createAndMint = (
+  context: Parameters<typeof createV1>[0],
+  input: CreateV1InstructionInput & MintV1InstructionInput
+): WrappedInstruction[] => [createV1(context, input), mintV1(context, input)];
+
+export const createNft = (
+  context: Parameters<typeof createAndMint>[0],
+  input: Omit<Parameters<typeof createAndMint>[1], 'amount' | 'tokenStandard'>
+): WrappedInstruction[] =>
+  createAndMint(context, {
+    ...input,
+    tokenStandard: TokenStandard.NonFungible,
+    amount: 1,
+  });
+
+export const createProgrammableNft = (
+  context: Parameters<typeof createAndMint>[0],
+  input: Omit<Parameters<typeof createAndMint>[1], 'amount' | 'tokenStandard'>
+): WrappedInstruction[] =>
+  createAndMint(context, {
+    ...input,
+    tokenStandard: TokenStandard.ProgrammableNonFungible,
+    amount: 1,
+  });
+
+export const createFungible = (
+  context: Parameters<typeof createV1>[0],
+  input: Omit<Parameters<typeof createV1>[1], 'tokenStandard'>
+): WrappedInstruction =>
+  createV1(context, {
+    ...input,
+    tokenStandard: TokenStandard.Fungible,
+  });
+
+export const createFungibleAsset = (
+  context: Parameters<typeof createV1>[0],
+  input: Omit<Parameters<typeof createV1>[1], 'tokenStandard'>
+): WrappedInstruction =>
+  createV1(context, {
+    ...input,
+    tokenStandard: TokenStandard.FungibleAsset,
+  });
--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,6 +1,7 @@
 export * from './generated';
 export * from './instructions';
 
+export * from './createHelpers';
 export * from './digitalAsset';
 export * from './digitalAssetWithToken';
 export * from './plugin';
