0ec7bc63d8d28882c89fd3cb125f48cef93b3ae3
Loris Leiva <loris.leiva@gmail.com>
2023-04-24 17:01:00 +0100

    Add more transfer tests

--- clients/js/test/transferV1.test.ts
+++ clients/js/test/transferV1.test.ts
@@ -85,3 +85,83 @@ test('it can transfer a ProgrammableNonFungible', async (t) => {
     },
   });
 });
+
+test('it can transfer a Fungible', async (t) => {
+  // Given a Fungible such that owner A owns 42 tokens.
+  const umi = await createUmi();
+  const ownerA = generateSigner(umi);
+  const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
+    tokenOwner: ownerA.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+    amount: 42,
+  });
+
+  // When we transfer 10 tokens to owner B.
+  const ownerB = generateSigner(umi).publicKey;
+  await transferV1(umi, {
+    mint,
+    authority: ownerA,
+    tokenOwner: ownerA.publicKey,
+    destinationOwner: ownerB,
+    tokenStandard: TokenStandard.Fungible,
+    amount: 10,
+  }).sendAndConfirm(umi);
+
+  // Then owner A has 32 tokens
+  const assetA = await fetchDigitalAssetWithAssociatedToken(
+    umi,
+    mint,
+    ownerA.publicKey
+  );
+  t.like(assetA, <DigitalAssetWithToken>{
+    mint: { publicKey: publicKey(mint), supply: 42n },
+    token: { owner: publicKey(ownerA), amount: 32n },
+  });
+
+  // And owner B has 10 tokens.
+  const assetB = await fetchDigitalAssetWithAssociatedToken(umi, mint, ownerB);
+  t.like(assetB, <DigitalAssetWithToken>{
+    mint: { publicKey: publicKey(mint), supply: 42n },
+    token: { owner: publicKey(ownerB), amount: 10n },
+  });
+});
+
+test('it can transfer a FungibleAsset', async (t) => {
+  // Given a FungibleAsset such that owner A owns 42 tokens.
+  const umi = await createUmi();
+  const ownerA = generateSigner(umi);
+  const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
+    tokenOwner: ownerA.publicKey,
+    tokenStandard: TokenStandard.FungibleAsset,
+    amount: 42,
+  });
+
+  // When we transfer 10 tokens to owner B.
+  const ownerB = generateSigner(umi).publicKey;
+  await transferV1(umi, {
+    mint,
+    authority: ownerA,
+    tokenOwner: ownerA.publicKey,
+    destinationOwner: ownerB,
+    tokenStandard: TokenStandard.FungibleAsset,
+    amount: 10,
+  }).sendAndConfirm(umi);
+
+  // Then owner A has 32 tokens
+  const assetA = await fetchDigitalAssetWithAssociatedToken(
+    umi,
+    mint,
+    ownerA.publicKey
+  );
+  t.like(assetA, <DigitalAssetWithToken>{
+    mint: { publicKey: publicKey(mint), supply: 42n },
+    token: { owner: publicKey(ownerA), amount: 32n },
+  });
+
+  // And owner B has 10 tokens.
+  const assetB = await fetchDigitalAssetWithAssociatedToken(umi, mint, ownerB);
+  t.like(assetB, <DigitalAssetWithToken>{
+    mint: { publicKey: publicKey(mint), supply: 42n },
+    token: { owner: publicKey(ownerB), amount: 10n },
+  });
+});
