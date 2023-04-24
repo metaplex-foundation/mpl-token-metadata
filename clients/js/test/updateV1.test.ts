6bc4de756b238faa0a28b2ba23e4ac95aae2bd94
Loris Leiva <loris.leiva@gmail.com>
2023-04-24 14:13:46 +0100

    Update updateV1.test.ts

--- clients/js/test/updateV1.test.ts
+++ clients/js/test/updateV1.test.ts
@@ -1,6 +1,12 @@
 import { some } from '@metaplex-foundation/umi';
 import test from 'ava';
-import { fetchMetadata, findMetadataPda, Metadata, updateV1 } from '../src';
+import {
+  fetchMetadata,
+  findMetadataPda,
+  Metadata,
+  TokenStandard,
+  updateV1,
+} from '../src';
 import { createDigitalAsset, createUmi } from './_setup';
 
 test('it can update a NonFungible', async (t) => {
@@ -11,6 +17,27 @@ test('it can update a NonFungible', async (t) => {
   const initialMetadataAccount = await fetchMetadata(umi, initialMetadata);
 
   // When we update the name of the NFT.
+  await updateV1(umi, {
+    mint: mint.publicKey,
+    data: some({ ...initialMetadataAccount, name: 'NFT #2' }),
+  }).sendAndConfirm(umi);
+
+  // Then the account data was updated.
+  const updatedMetadataAccount = await fetchMetadata(umi, initialMetadata);
+  t.like(updatedMetadataAccount, <Metadata>{ name: 'NFT #2' });
+});
+
+test('it can update a ProgrammableNonFungible', async (t) => {
+  // Given an existing PNFT.
+  const umi = await createUmi();
+  const mint = await createDigitalAsset(umi, {
+    name: 'NFT #1',
+    tokenStandard: TokenStandard.ProgrammableNonFungible,
+  });
+  const initialMetadata = findMetadataPda(umi, { mint: mint.publicKey });
+  const initialMetadataAccount = await fetchMetadata(umi, initialMetadata);
+
+  // When we update the name of the PNFT.
   await updateV1(umi, {
     mint: mint.publicKey,
     data: some({ ...initialMetadataAccount, name: 'NFT #2' }),
