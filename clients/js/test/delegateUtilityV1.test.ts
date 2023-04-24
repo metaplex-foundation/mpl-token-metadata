ecb09452e6acb3229ec7ab0a59159dda6631b18b
Loris Leiva <loris.leiva@gmail.com>
2023-04-24 19:32:38 +0100

    wip

--- clients/js/test/delegateUtilityV1.test.ts
+++ clients/js/test/delegateUtilityV1.test.ts
@@ -1,45 +1,43 @@
-import { findAssociatedTokenPda } from '@metaplex-foundation/mpl-essentials';
-import { PublicKey, generateSigner, publicKey } from '@metaplex-foundation/umi';
+import { generateSigner, publicKey } from '@metaplex-foundation/umi';
 import test from 'ava';
 import {
   DigitalAssetWithToken,
   TokenStandard,
+  delegateUtilityV1,
   fetchDigitalAssetWithAssociatedToken,
-  transferV1,
 } from '../src';
 import { createDigitalAssetWithToken, createUmi } from './_setup';
 
-test('it can delegate a NonFungible', async (t) => {
-  // Given a NonFungible that belongs to owner A.
+test('it can approve a utility delegate for a NonFungible', async (t) => {
+  // Given a NonFungible.
   const umi = await createUmi();
-  const ownerA = generateSigner(umi);
+  const owner = generateSigner(umi);
   const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
-    tokenOwner: ownerA.publicKey,
+    tokenOwner: owner.publicKey,
   });
 
-  // When we transfer the asset to owner B.
-  const ownerB = generateSigner(umi).publicKey;
-  await transferV1(umi, {
+  // When we approve a utility delegate.
+  const utilityDelegate = generateSigner(umi).publicKey;
+  await delegateUtilityV1(umi, {
     mint,
-    authority: ownerA,
-    tokenOwner: ownerA.publicKey,
-    destinationOwner: ownerB,
+    authority: owner,
+    delegate: utilityDelegate,
     tokenStandard: TokenStandard.NonFungible,
   }).sendAndConfirm(umi);
 
   // Then the asset is now owned by owner B.
-  const da = await fetchDigitalAssetWithAssociatedToken(umi, mint, ownerB);
+  const da = await fetchDigitalAssetWithAssociatedToken(
+    umi,
+    mint,
+    owner.publicKey
+  );
   t.like(da, <DigitalAssetWithToken>{
     mint: {
       publicKey: publicKey(mint),
       supply: 1n,
     },
     token: {
-      publicKey: findAssociatedTokenPda(umi, {
-        mint,
-        owner: ownerB,
-      }) as PublicKey,
-      owner: ownerB,
+      owner: owner.publicKey,
       amount: 1n,
     },
   });
