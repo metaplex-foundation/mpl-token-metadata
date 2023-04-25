3d3e656420e83ab10a92e60743fddb35e73c2119
Loris Leiva <loris.leiva@gmail.com>
2023-04-25 14:32:09 +0100

    Update delegateCollectionV1.test.ts

--- clients/js/test/delegateCollectionV1.test.ts
+++ clients/js/test/delegateCollectionV1.test.ts
@@ -80,3 +80,75 @@ test('it can approve a collection delegate for a ProgrammableNonFungible', async
     delegate: publicKey(collectionDelegate),
   });
 });
+
+test('it can approve a collection delegate for a Fungible', async (t) => {
+  // Given a Fungible.
+  const umi = await createUmi();
+  const updateAuthority = generateSigner(umi);
+  const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
+    authority: updateAuthority,
+    tokenStandard: TokenStandard.Fungible,
+  });
+
+  // When we approve a collection delegate.
+  const collectionDelegate = generateSigner(umi).publicKey;
+  await delegateCollectionV1(umi, {
+    mint,
+    updateAuthority: updateAuthority.publicKey,
+    authority: updateAuthority,
+    delegate: collectionDelegate,
+    tokenStandard: TokenStandard.Fungible,
+  }).sendAndConfirm(umi);
+
+  // Then a new metadata delegate record was created.
+  const delegateRecord = await fetchMetadataDelegateRecord(
+    umi,
+    findMetadataDelegateRecordPda(umi, {
+      mint,
+      delegateRole: MetadataDelegateRole.Collection,
+      delegate: collectionDelegate,
+      updateAuthority: updateAuthority.publicKey,
+    })
+  );
+  t.like(delegateRecord, <MetadataDelegateRecord>{
+    mint: publicKey(mint),
+    updateAuthority: publicKey(updateAuthority),
+    delegate: publicKey(collectionDelegate),
+  });
+});
+
+test('it can approve a collection delegate for a FungibleAsset', async (t) => {
+  // Given a FungibleAsset.
+  const umi = await createUmi();
+  const updateAuthority = generateSigner(umi);
+  const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
+    authority: updateAuthority,
+    tokenStandard: TokenStandard.FungibleAsset,
+  });
+
+  // When we approve a collection delegate.
+  const collectionDelegate = generateSigner(umi).publicKey;
+  await delegateCollectionV1(umi, {
+    mint,
+    updateAuthority: updateAuthority.publicKey,
+    authority: updateAuthority,
+    delegate: collectionDelegate,
+    tokenStandard: TokenStandard.FungibleAsset,
+  }).sendAndConfirm(umi);
+
+  // Then a new metadata delegate record was created.
+  const delegateRecord = await fetchMetadataDelegateRecord(
+    umi,
+    findMetadataDelegateRecordPda(umi, {
+      mint,
+      delegateRole: MetadataDelegateRole.Collection,
+      delegate: collectionDelegate,
+      updateAuthority: updateAuthority.publicKey,
+    })
+  );
+  t.like(delegateRecord, <MetadataDelegateRecord>{
+    mint: publicKey(mint),
+    updateAuthority: publicKey(updateAuthority),
+    delegate: publicKey(collectionDelegate),
+  });
+});
