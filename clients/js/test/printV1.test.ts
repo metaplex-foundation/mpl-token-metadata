f3c1a59a99f1e40691a3f317f319f0e2df7b4ef1
Loris Leiva <loris.leiva@gmail.com>
2023-08-18 11:12:52 +0100

    Ensure we cannot initialize an edition mint with more than 1 token

--- clients/js/test/printV1.test.ts
+++ clients/js/test/printV1.test.ts
@@ -82,3 +82,39 @@ test('it can print a new edition from a NonFungible by initializing the mint bef
   // And the printed NFT was created with the same data.
   // TODO
 });
+
+test('it cannot print a new edition if the initialized edition mint account has more than 1 token of supply', async (t) => {
+  // Given an existing master edition asset.
+  const umi = await createUmi();
+  const originalOwner = generateSigner(umi);
+  const originalMint = await createDigitalAssetWithToken(umi, {
+    tokenOwner: originalOwner.publicKey,
+    printSupply: printSupply('Limited', [10]),
+    tokenStandard: TokenStandard.NonFungible,
+  });
+
+  // And given we have the mint of the printed edition initialized with more than 1 token.
+  const editionMint = generateSigner(umi);
+  const editionMintAuthority = generateSigner(umi);
+  const editionOwner = generateSigner(umi);
+  await createMintWithAssociatedToken(umi, {
+    mint: editionMint,
+    owner: editionOwner.publicKey,
+    mintAuthority: editionMintAuthority,
+    freezeAuthority: editionMintAuthority.publicKey,
+    amount: 2,
+  }).sendAndConfirm(umi);
+
+  // When we try to print a new edition of the asset.
+  const promise = printV1(umi, {
+    masterTokenAccountOwner: originalOwner,
+    masterEditionMint: originalMint.publicKey,
+    editionMint,
+    editionMintAuthority,
+    editionTokenAccountOwner: editionOwner.publicKey,
+    editionNumber: 1,
+  }).sendAndConfirm(umi);
+
+  // Then we expect a program error.
+  await t.throwsAsync(promise, { name: 'InvalidMintForTokenStandard' });
+});
