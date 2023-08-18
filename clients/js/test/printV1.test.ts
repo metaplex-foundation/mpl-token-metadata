bcb21e3ae725d29d12ac0d36027443e5b6d4e327
Loris Leiva <loris.leiva@gmail.com>
2023-08-18 11:27:12 +0100

    Finish printV1 tests

--- clients/js/test/printV1.test.ts
+++ clients/js/test/printV1.test.ts
@@ -1,26 +1,33 @@
 import { createMintWithAssociatedToken } from '@metaplex-foundation/mpl-toolbox';
-import { generateSigner, some } from '@metaplex-foundation/umi';
+import { generateSigner, percentAmount, some } from '@metaplex-foundation/umi';
 import test from 'ava';
 import {
   DigitalAsset,
+  DigitalAssetWithToken,
   TokenStandard,
   fetchDigitalAsset,
+  fetchDigitalAssetWithAssociatedToken,
+  findMasterEditionPda,
   printSupply,
   printV1,
 } from '../src';
 import { createDigitalAssetWithToken, createUmi } from './_setup';
 
-test.skip('it can print a new edition from a NonFungible', async (t) => {
+test('it can print a new edition from a NonFungible', async (t) => {
   // Given an existing master edition asset.
   const umi = await createUmi();
   const originalOwner = generateSigner(umi);
   const originalMint = await createDigitalAssetWithToken(umi, {
+    name: 'My NFT',
+    symbol: 'MNFT',
+    uri: 'https://example.com/nft.json',
+    sellerFeeBasisPoints: percentAmount(5.42),
     tokenOwner: originalOwner.publicKey,
     printSupply: printSupply('Limited', [10]),
     tokenStandard: TokenStandard.NonFungible,
   });
 
-  // When we update the name of the asset.
+  // When we print a new edition of the asset.
   const editionMint = generateSigner(umi);
   const editionOwner = generateSigner(umi);
   await printV1(umi, {
@@ -38,7 +45,29 @@ test.skip('it can print a new edition from a NonFungible', async (t) => {
   });
 
   // And the printed NFT was created with the same data.
-  // TODO
+  const editionAsset = await fetchDigitalAssetWithAssociatedToken(
+    umi,
+    editionMint.publicKey,
+    editionOwner.publicKey
+  );
+  t.like(editionAsset, <DigitalAssetWithToken>{
+    publicKey: editionMint.publicKey,
+    metadata: {
+      name: 'My NFT',
+      symbol: 'MNFT',
+      uri: 'https://example.com/nft.json',
+      sellerFeeBasisPoints: 542,
+    },
+    token: {
+      owner: editionOwner.publicKey,
+      amount: 1n,
+    },
+    edition: {
+      isOriginal: false,
+      parent: findMasterEditionPda(umi, { mint: originalMint.publicKey })[0],
+      edition: 1n,
+    },
+  });
 });
 
 test('it can print a new edition from a NonFungible by initializing the mint beforehand', async (t) => {
@@ -46,6 +75,10 @@ test('it can print a new edition from a NonFungible by initializing the mint bef
   const umi = await createUmi();
   const originalOwner = generateSigner(umi);
   const originalMint = await createDigitalAssetWithToken(umi, {
+    name: 'My NFT',
+    symbol: 'MNFT',
+    uri: 'https://example.com/nft.json',
+    sellerFeeBasisPoints: percentAmount(5.42),
     tokenOwner: originalOwner.publicKey,
     printSupply: printSupply('Limited', [10]),
     tokenStandard: TokenStandard.NonFungible,
@@ -63,7 +96,7 @@ test('it can print a new edition from a NonFungible by initializing the mint bef
     amount: 1,
   }).sendAndConfirm(umi);
 
-  // When we update the name of the asset.
+  // When we print a new edition of the asset.
   await printV1(umi, {
     masterTokenAccountOwner: originalOwner,
     masterEditionMint: originalMint.publicKey,
@@ -76,11 +109,37 @@ test('it can print a new edition from a NonFungible by initializing the mint bef
   // Then the original NFT was updated.
   const originalAsset = await fetchDigitalAsset(umi, originalMint.publicKey);
   t.like(originalAsset, <DigitalAsset>{
-    edition: { supply: 1n, maxSupply: some(10n) },
+    edition: {
+      isOriginal: true,
+      supply: 1n,
+      maxSupply: some(10n),
+    },
   });
 
   // And the printed NFT was created with the same data.
-  // TODO
+  const editionAsset = await fetchDigitalAssetWithAssociatedToken(
+    umi,
+    editionMint.publicKey,
+    editionOwner.publicKey
+  );
+  t.like(editionAsset, <DigitalAssetWithToken>{
+    publicKey: editionMint.publicKey,
+    metadata: {
+      name: 'My NFT',
+      symbol: 'MNFT',
+      uri: 'https://example.com/nft.json',
+      sellerFeeBasisPoints: 542,
+    },
+    token: {
+      owner: editionOwner.publicKey,
+      amount: 1n,
+    },
+    edition: {
+      isOriginal: false,
+      parent: findMasterEditionPda(umi, { mint: originalMint.publicKey })[0],
+      edition: 1n,
+    },
+  });
 });
 
 test('it cannot print a new edition if the initialized edition mint account has more than 1 token of supply', async (t) => {
