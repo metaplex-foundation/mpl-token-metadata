6a2627076f4f3c50edf48e0143411fd9b1463c86
Fernando Otero <febo@metaplex.com>
2023-08-20 15:50:59 +0300

    Fix program name

--- clients/js/src/plugin.ts
+++ clients/js/src/plugin.ts
@@ -1,10 +1,10 @@
 import { UmiPlugin } from '@metaplex-foundation/umi';
 import { mplToolbox } from '@metaplex-foundation/mpl-toolbox';
-import { createMplTokenMetadataProgram } from './generated';
+import { createTokenMetadataProgram } from './generated';
 
 export const mplTokenMetadata = (): UmiPlugin => ({
   install(umi) {
     umi.use(mplToolbox());
-    umi.programs.add(createMplTokenMetadataProgram(), false);
+    umi.programs.add(createTokenMetadataProgram(), false);
   },
 });
