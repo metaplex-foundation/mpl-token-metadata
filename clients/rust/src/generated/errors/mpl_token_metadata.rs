b088cda90a41743a4a996ecbf33c1266407325e1
febo <febo@users.noreply.github.com>
2023-08-31 20:47:56 +0000

    Update generated clients

--- clients/rust/src/generated/errors/mpl_token_metadata.rs
+++ clients/rust/src/generated/errors/mpl_token_metadata.rs
@@ -5,10 +5,9 @@
 //! [https://github.com/metaplex-foundation/kinobi]
 //!
 
-use num_derive::FromPrimitive;
-use thiserror::Error;
+use spl_program_error::*;
 
-#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+#[spl_program_error]
 pub enum MplTokenMetadataError {
     /// 0x0 -
     #[error("")]
@@ -589,10 +588,4 @@ pub enum MplTokenMetadataError {
     /// 0xC0 -
     #[error("")]
     InvalidMetadataFlags,
-}
-
-impl solana_program::program_error::PrintProgramError for MplTokenMetadataError {
-    fn print<E>(&self) {
-        solana_program::msg!(&self.to_string());
-    }
 }
