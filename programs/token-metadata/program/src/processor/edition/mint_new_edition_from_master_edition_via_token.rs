0dc5f0d196940c7a757f9eaacf851ac78a28b0b8
febo <febo@metaplex.com>
2023-09-12 15:40:51 +0100

    Add token program test

--- programs/token-metadata/program/src/processor/edition/mint_new_edition_from_master_edition_via_token.rs
+++ programs/token-metadata/program/src/processor/edition/mint_new_edition_from_master_edition_via_token.rs
@@ -1,6 +1,7 @@
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 
 use crate::{
+    error::MetadataError,
     processor::all_account_infos,
     utils::{
         fee::{levy, set_fee_flag, LevyArgs},
@@ -30,6 +31,10 @@ pub fn process_mint_new_edition_from_master_edition_via_token<'a>(
         token_program_account_info,
         system_account_info
     );
+
+    if token_program_account_info.key == &spl_token_2022::ID {
+        return Err(MetadataError::InvalidTokenProgram.into());
+    }
 
     // Levy fees first, to fund the metadata account with rent + fee amount.
     levy(LevyArgs {
