5a63782c263072a274b90f2959ed3adacd711b74
Fernando Otero <febo@metaplex.com>
2023-09-22 08:57:46 +0100

    Add default impl to types (#56)
    * Separate utils module
    
    * Add default trait impl
    
    * Update tests
    
    * Add helpers

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -1,10 +1,7 @@
 mod generated;
 pub mod hooked;
+mod traits;
+pub mod utils;
 
 pub use generated::programs::MPL_TOKEN_METADATA_ID as ID;
 pub use generated::*;
-
-/// Removes all null bytes from a string.
-pub fn clean(value: String) -> String {
-    value.replace('\0', "")
-}
