ec54bbcf06030d8e832709137ac7ca2643d80fc2
febo <febo@metaplex.com>
2023-11-17 22:56:06 +0000

    Add key check

--- clients/rust/src/utils.rs
+++ clients/rust/src/utils.rs
@@ -1,4 +1,9 @@
-use crate::{errors::MplTokenMetadataError, types::TokenStandard};
+use borsh::BorshDeserialize;
+
+use crate::{
+    errors::MplTokenMetadataError,
+    types::{Key, TokenStandard},
+};
 
 /// The offset of the token standard byte in the master edition
 /// from the end of the account data.
@@ -11,12 +16,29 @@ pub fn clean(value: String) -> String {
 
 /// Checks that the `master_edition` is Programmable NFT master edition.
 pub fn assert_edition_is_programmable(edition_data: &[u8]) -> Result<(), MplTokenMetadataError> {
-    if !edition_data.is_empty()
-        && edition_data[edition_data.len() - TOKEN_STANDARD_OFFSET]
-            == TokenStandard::ProgrammableNonFungible as u8
-    {
-        Ok(())
-    } else {
-        Err(MplTokenMetadataError::InvalidTokenStandard)
+    if !edition_data.is_empty() {
+        // the first byte is the account key
+        let key = Key::deserialize(&mut &edition_data[0..1])
+            .map_err(|_error| MplTokenMetadataError::InvalidEditionKey)?;
+
+        if matches!(key, Key::MasterEditionV1 | Key::MasterEditionV2) {
+            // the last byte is the token standard
+            let standard = TokenStandard::deserialize(
+                &mut &edition_data[edition_data.len() - TOKEN_STANDARD_OFFSET..],
+            )
+            .map_err(|_error| MplTokenMetadataError::InvalidTokenStandard)?;
+
+            return if matches!(
+                standard,
+                TokenStandard::ProgrammableNonFungible
+                    | TokenStandard::ProgrammableNonFungibleEdition
+            ) {
+                Ok(())
+            } else {
+                Err(MplTokenMetadataError::InvalidTokenStandard)
+            };
+        }
     }
+
+    Err(MplTokenMetadataError::DataIsEmptyOrZeroed)
 }
