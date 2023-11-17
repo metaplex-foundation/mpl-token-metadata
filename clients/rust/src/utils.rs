f992abf1d936b3c81dcaac5905678bf9e153dd48
febo <febo@metaplex.com>
2023-11-17 17:04:14 +0000

    Add helper

--- clients/rust/src/utils.rs
+++ clients/rust/src/utils.rs
@@ -1,4 +1,22 @@
+use crate::{errors::MplTokenMetadataError, types::TokenStandard};
+
+/// The offset of the token standard byte in the master edition
+/// from the end of the account data.
+const TOKEN_STANDARD_OFFSET: usize = 1;
+
 /// Removes all null bytes from a string.
 pub fn clean(value: String) -> String {
     value.replace('\0', "")
+}
+
+/// Checks that the `master_edition` is Programmable NFT master edition.
+pub fn assert_edition_is_programmable(edition_data: &[u8]) -> Result<(), MplTokenMetadataError> {
+    if !edition_data.is_empty()
+        && edition_data[edition_data.len() - TOKEN_STANDARD_OFFSET]
+            == TokenStandard::ProgrammableNonFungible as u8
+    {
+        Ok(())
+    } else {
+        Err(MplTokenMetadataError::InvalidTokenStandard)
+    }
 }
