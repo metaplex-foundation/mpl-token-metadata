c69d4ff60f0dceff612979db6da47c3bf098f5fb
febo <febo@metaplex.com>
2023-11-18 11:15:04 +0000

    Use specific length error

--- clients/rust/src/utils.rs
+++ clients/rust/src/utils.rs
@@ -40,5 +40,5 @@ pub fn assert_edition_is_programmable(edition_data: &[u8]) -> Result<(), MplToke
         }
     }
 
-    Err(MplTokenMetadataError::InvalidTokenStandard)
+    Err(MplTokenMetadataError::InvalidMasterEditionAccountLength)
 }
