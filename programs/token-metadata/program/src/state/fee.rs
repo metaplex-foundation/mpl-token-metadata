2fb1e637b733de00566a2387aecec4beb169433b
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-10-28 15:32:04 -0700

    Update fee destination address

--- programs/token-metadata/program/src/state/fee.rs
+++ programs/token-metadata/program/src/state/fee.rs
@@ -2,6 +2,7 @@ use super::*;
 use solana_program::{rent::Rent, sysvar::Sysvar};
 
 pub(crate) const FEE_AUTHORITY: Pubkey = pubkey!("Levytx9LLPzAtDJJD7q813Zsm8zg9e1pb53mGxTKpD7");
+pub const FEE_DESTINATION: Pubkey = pubkey!("2fb1TjRrJQLy9BkYfBjcYgibV7LUsr9cf6QxvyRZyuXn");
 pub(crate) const OWNERLESS_CLOSE_AUTHORITY: Pubkey =
     pubkey!("C1oseLQExhuEzeBhsVbLtseSpVgvpHDbBj3PTevBCEBh");
 pub(crate) const OWNERLESS_CLOSE_DESTINATION: Pubkey =
